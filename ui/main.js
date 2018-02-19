34eefe7401da970856c639cfaea83dfcba4e8919
chiragatgit18 <36497823+chiragatgit18@users.noreply.github.com>
2018-02-19 20:29:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="This is NEW TEXT";
 
-var img=document.getElementById('madi');
+var img=document.getElementById('img');
 img.onClick=function(){
     img.style.marginLeft= '100px';
 }
