c0c3aef4325dea3c3922bf4d615a7ed131476de3
chiragatgit18 <36497823+chiragatgit18@users.noreply.github.com>
2018-02-20 23:51:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onClick=function()
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200)
             {
-                var counter=requst.responseText;
+                var counter=request.responseText;
                 var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
                 
