00ae98945ba7b9c16db54be8f7b75e069793a93c
chiragatgit18 <36497823+chiragatgit18@users.noreply.github.com>
2018-02-20 23:58:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,9 +92,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/counter', function (req, res) {
-  res.sendFile(path.join(counter));
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
