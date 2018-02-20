69bc68a795caee1caecef9d324ca91343fee3364
chiragatgit18 <36497823+chiragatgit18@users.noreply.github.com>
2018-02-20 23:42:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,33 @@
-console.log('Loaded!');
+// counter code
 
+var button=document.getElementById('counter');
 
-//var element=document.getElementById('main-text');
-//element.innerHTML="This is NEW TEXT";
-
-var img=document.getElementById('madi');
-img.onClick= function(){
-    img.style.marginLeft= '200px';
-}
+button.onClick=function()
+{
+    //create a Request Object
+    
+    var request= new XMLHttpRequest();
+    
+    // capture a response store it in a variable
+    
+    request.onreadystatechange=== function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200)
+            {
+                var counter=requst.responseText;
+                var span=getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+        }f
+            
+    };
+    
+    // Make a request
+    
+    request.open('GET','http://chiraggambha15197.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
+};
